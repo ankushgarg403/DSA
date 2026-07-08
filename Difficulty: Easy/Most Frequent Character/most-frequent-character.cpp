6a75da55297475e71c22edfadc89530c751a30ce
Ankush Garg <ankushgarg01106@gmail.com>
2026-07-08 18:15:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Most Frequent Character/most-frequent-character.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    char getMaxOccuringChar(string& s) {
+        int arr[26] = {0};
+        
+        for(int i = 0 ; i<s.length() ; i++){
+            char ch = s[i];
+            int number = 0;
+            number = ch - 'a';
+            arr[number]++;
+        }
+        
+        int maxi = -1;
+        int ans = 0;
+        for(int i = 0 ; i < 26 ; i++){
+            if(maxi < arr[i]){
+                ans = i;
+                maxi = arr[i];
+            }
+        }
+        return 'a'+ans;
+    }
+};
