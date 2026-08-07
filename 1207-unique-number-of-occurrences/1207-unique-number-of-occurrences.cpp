06ad4733971c97336316fa3754577b98de02070f
Ankush Garg <ankushgarg01106@gmail.com>
2026-08-07 09:52:45 +0530

    Time: 0 ms (100%), Space: 11.7 MB (97.68%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
         vector<int> ans;
-        for (int i = 0 ; i < arr.size() ; i++){
-            int count = 1;
-            while(i+1 < arr.size() && arr[i] == arr[i+1]){
-                count++;
-                i++;
-            }
-            ans.push_back(count);
+        if(arr.size() == 1){
+            return true;
         }
-        sort(ans.begin(),ans.end());
 
-        for (int i = 1 ; i < ans.size() ; i++){
-            if(ans[i] == ans[i-1]){
+        sort(arr.begin() , arr.end());
+
+        int i = 0;
+        int j = 1;
+        while(i < arr.size() && j < arr.size()){
+            if(arr[i] == arr[j]){
+                j++;
+                if(j > arr.size() - 1){
+                    ans.push_back(j-i);
+                }
+            }
+            else{
+                ans.push_back(j-i);
+                j++;
+                i = j - 1;
+                if(j > arr.size() - 1){
+                    if(arr[j-2] == arr[j-1]){
+                        ans.push_back(j-i);
+                    }
+                    else{
+                        ans.push_back(1);
+                    }
+                }
+            }
+        }
+
+        sort(ans.begin() , ans.end());
+        for(int k = 0 ; k < ans.size()-1 ; k++){
+            if(ans[k] == ans[k+1]){
                 return false;
             }
         }
