4ec2767ca379868f0eac256c968ece7a4dee1495
Ankush Garg <ankushgarg01106@gmail.com>
2026-08-21 12:12:08 +0530

    Time: 18 ms (5.11%), Space: 13.5 MB (9.73%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -11,36 +11,22 @@ public:
     ListNode *detectCycle(ListNode *head) {
         map<ListNode*,bool> m;
         
-        if(head == NULL || head -> next == NULL){
-            return NULL;
-        }
+        // if(head == NULL || head -> next == NULL){
+        //     return NULL;
+        // }
 
-        ListNode* i = 0;
         ListNode* temp = head;
         while(temp != NULL){
             if(temp -> next == NULL){
                 return NULL;
             }
             if(m[temp]){
-                i = temp;
-                break;
+                return temp;
             }
             m[temp] = true;
             temp = temp -> next;
         }
-
-        ListNode* pos = head;
-        // for(int j = 0 ; j <= 10000 ; j++){
-        //     if(pos == i){
-        //         return pos;
-        //     }
-        //     pos = pos -> next;
-        // }
-
-        while(pos != i){
-            pos = pos -> next;
-        }
-        return pos;
+        return temp;;
 
     }
 };
