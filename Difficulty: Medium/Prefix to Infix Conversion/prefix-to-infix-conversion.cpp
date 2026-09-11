536e16f8ea6e97f1d6603fb1ccde35d3c739563d
Ankush Garg <ankushgarg01106@gmail.com>
2026-09-11 10:23:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    string preToInfix(string &s) {
+        stack<string> st;
+        
+        int n = s.length();
+        
+        int i = n - 1;
+        while(i >= 0){
+            char el = s[i];
+            // string ch = to_string(s[i]);
+            string ch(1, s[i]);
+            if((el >= 'A' && el <= 'Z') || 
+                (el >= 'a' && el <= 'z') || 
+                (el >= '0' && el <= '9'))
+                {
+                    st.push(ch);
+                }
+            
+            else{
+                string b = st.top();
+                st.pop();
+                
+                string a = st.top();
+                st.pop();
+                
+                string temp = '(' + b + el + a + ')';
+                
+                st.push(temp);
+                
+            }
+            i--;
+        }
+        string ans = st.top();
+        return ans;
+    }
+};
