4f5fc7cd867be1b1f18851b1e4d7d1d88c7c44c3
Ankush Garg <ankushgarg01106@gmail.com>
2026-09-12 11:45:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        stack<int> st;
+        st.push(-1);
+        
+        int n = arr.size();
+        
+        vector<int> ans(n);
+        
+        int i = n - 1;
+        
+        while(i >= 0){
+            int el = arr[i];
+            while(st.top() <= el){
+                if(st.top() == -1){
+                    // ans[i] = -1;
+                    // st.push(el);
+                    break;
+                }
+                st.pop();
+            }
+            ans[i] = st.top();
+            st.push(el);
+            i--;
+        }
+        
+        return ans;
+    }
+};
