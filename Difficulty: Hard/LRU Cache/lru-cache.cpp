4778698be3fa83174506970c7fe6f29bf27d746d
Ankush Garg <ankushgarg01106@gmail.com>
2026-09-19 16:24:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/LRU Cache/lru-cache.cpp
@@ -0,0 +1,76 @@
+class LRUCache {
+private:
+    class Node{
+    public:
+        int key,value;
+        Node* prev;
+        Node* next;
+
+        Node(int key , int value){
+            this -> key = key;
+            this -> value = value;
+            prev = nullptr;
+            next = nullptr;
+        }
+    };
+
+    int capacity;
+    map<int,Node*> m;
+    Node* head;
+    Node* tail;
+
+    void deletenode(Node* node){
+        Node* prevnode = node -> prev;
+        Node* afternode = node -> next;
+
+        prevnode -> next = afternode;
+        afternode -> prev = prevnode;
+    }
+
+    void insertathead(Node* node){
+        Node* currafternode = head -> next;
+        head->next = node;
+        node -> prev = head;
+        node -> next = currafternode;
+        currafternode -> prev = node;
+    }
+  public:
+    LRUCache(int cap) {
+        this -> capacity = cap;
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if(m.find(key) == m.end()) return -1;
+            Node* node = m[key];
+            deletenode(node);
+            insertathead(node);
+    
+            return node -> value;
+        }
+
+        
+    void put(int key, int value) {
+        if(m.find(key) != m.end()){
+            Node* node = m[key];
+            node -> value = value;
+            deletenode(node);
+            insertathead(node);
+        }
+        else{
+            if(m.size() == capacity){
+                Node* node = tail->prev;
+                m.erase(node -> key);
+                deletenode(node);
+            }
+            Node* node = new Node(key,value);
+            m[key] = node;
+            insertathead(node);
+        }
+    }
+};
