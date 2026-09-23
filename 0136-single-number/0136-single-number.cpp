3dc8a0fd6595575a9cfe96a7fff156adeb085403
Ankush Garg <ankushgarg01106@gmail.com>
2026-09-23 18:18:03 +0530

    Time: 0 ms (100%), Space: 20.5 MB (83.7%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // int n = nums.size();
+        // int sum = 0;
+        
+        for(int i = 1 ; i < nums.size() ; i++){
+            nums[0] ^= nums[i];
+        }
+        return nums[0];
+    }
+};
